37026e8eb4ab8bb8777776e063fc21578350488e
Edvinas <edvinas108@gmail.com>
2021-02-05 20:11:48 +0200

    Add bootstrap scene loading, add initial scene loading events

---
+++ Assets/Scripts/Editor/BootstrapSceneLoader.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using System.Linq;
+using NeanderthalTools.Scenes;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NeanderthalTools.Editor
+{
+    [InitializeOnLoad]
+    public static class BootstrapSceneLoader
+    {
+        #region Fields
+
+        private const string DefaultSceneSettingsDirectoryPath = "Assets/Settings/Scenes";
+
+        private const string DefaultSceneSettingsAssetPath =
+            DefaultSceneSettingsDirectoryPath + "/SceneSettings.asset";
+
+        private static readonly SceneSettings SceneSettings;
+
+        #endregion
+
+        #region Methods
+
+        static BootstrapSceneLoader()
+        {
+            SceneSettings = LoadSceneSettings();
+            EditorSceneManager.sceneOpened += SceneOpened;
+        }
+
+        private static SceneSettings LoadSceneSettings()
+        {
+            var sceneSettings = FindSceneSettings();
+            return sceneSettings != null
+                ? sceneSettings
+                : CreateSceneSettings();
+        }
+
+        private static SceneSettings FindSceneSettings()
+        {
+            return AssetDatabase
+                .FindAssets($"t:{nameof(Scenes.SceneSettings)}")
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<SceneSettings>)
+                .FirstOrDefault();
+        }
+
+        private static SceneSettings CreateSceneSettings()
+        {
+            var sceneSettings = ScriptableObject.CreateInstance<SceneSettings>();
+
+            Directory.CreateDirectory(DefaultSceneSettingsDirectoryPath);
+
+            AssetDatabase.CreateAsset(sceneSettings, DefaultSceneSettingsAssetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            return sceneSettings;
+        }
+
+        private static void SceneOpened(Scene scene, OpenSceneMode mode)
+        {
+            if (!scene.IsValid() || IsBootstrapScene(scene))
+            {
+                return;
+            }
+
+            var sceneSetups = CreateBootstrappedSceneSetups(scene);
+            EditorSceneManager.RestoreSceneManagerSetup(sceneSetups);
+        }
+
+        private static bool IsBootstrapScene(Scene scene)
+        {
+            return scene.buildIndex == SceneSettings.BootstrapSceneIndex;
+        }
+
+        private static SceneSetup[] CreateBootstrappedSceneSetups(Scene scene)
+        {
+            return new[]
+            {
+                new SceneSetup
+                {
+                    isActive = false,
+                    isLoaded = true,
+                    path = GetBootstrapScenePath()
+                },
+                new SceneSetup
+                {
+                    isActive = true,
+                    isLoaded = true,
+                    path = scene.path
+                }
+            };
+        }
+
+        private static string GetBootstrapScenePath()
+        {
+            return SceneUtility.GetScenePathByBuildIndex(SceneSettings.BootstrapSceneIndex);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Scenes/SceneSettings.cs
@@ -0,0 +1,29 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace NeanderthalTools.Scenes
+{
+    [CreateAssetMenu(fileName = "SceneSettings", menuName = "Game/Scene Settings")]
+    public class SceneSettings : ScriptableObject
+    {
+        #region Editor
+
+        [Scene]
+        [SerializeField]
+        private int bootstrapSceneIndex;
+
+        [Scene]
+        [SerializeField]
+        private int mainSceneIndex;
+
+        #endregion
+
+        #region Properties
+
+        public int BootstrapSceneIndex => bootstrapSceneIndex;
+
+        public int MainSceneIndex => mainSceneIndex;
+
+        #endregion
+    }
+}
