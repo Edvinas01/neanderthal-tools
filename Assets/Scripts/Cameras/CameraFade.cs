fda525672949053f78b758c1f617b586966a7597
Edvinas <edvinas108@gmail.com>
2021-02-07 10:59:16 +0200

    Add camera stacking for fade UI

---
+++ Assets/Scripts/Cameras/CameraFade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace NeanderthalTools.Cameras
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraFade : MonoBehaviour
+    {
+        private Camera camera;
+
+        private void Awake()
+        {
+            camera = GetComponent<Camera>();
+        }
+    }
+}
