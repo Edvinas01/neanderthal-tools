f1569f7e9d41865ff7396a42b8eaec3cc33ad7b7
Edvinas <edvinas108@gmail.com>
2021-03-31 15:34:56 +0300

    Raise state events on tool interactions, add counting trigger utils

---
+++ Assets/Scripts/Triggers/CountingTrigger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NeanderthalTools.Triggers
+{
+    public class CountingTrigger : MonoBehaviour
+    {
+        #region Editor
+
+        [Min(0)]
+        [SerializeField]
+        private int desiredCount;
+
+        [SerializeField]
+        private UnityEvent onTrigger;
+
+        #endregion
+
+        #region Fields
+
+        [NaughtyAttributes.ShowNonSerializedField]
+        private int currentCount;
+
+        #endregion
+
+        #region Unity Lifecycle
+
+        private void OnDisable()
+        {
+            currentCount = 0;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Increment()
+        {
+            if (IsDesiredCount())
+            {
+                return;
+            }
+
+            currentCount++;
+            if (IsDesiredCount())
+            {
+                onTrigger.Invoke();
+            }
+        }
+
+        public void Decrement()
+        {
+            if (IsDesiredCount())
+            {
+                return;
+            }
+
+            currentCount = Math.Max(currentCount - 1, 0);
+        }
+
+        private bool IsDesiredCount()
+        {
+            return currentCount == desiredCount;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Triggers/ScriptableEventLatchTrigger.cs
@@ -0,0 +1,73 @@
+﻿using ScriptableEvents;
+using ScriptableEvents.Simple;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NeanderthalTools.Triggers
+{
+    public class ScriptableEventLatchTrigger : MonoBehaviour, IScriptableEventListener<SimpleArg>
+    {
+        #region Editor
+
+        [SerializeField]
+        private SimpleScriptableEvent openTrigger;
+
+        [SerializeField]
+        private SimpleScriptableEvent closeTrigger;
+
+        [SerializeField]
+        private UnityEvent onOpen;
+
+        [SerializeField]
+        private UnityEvent onClose;
+
+        #endregion
+
+        #region Fields
+
+        private bool closed;
+
+        #endregion
+
+        #region Unity Lifecycle
+
+        private void OnEnable()
+        {
+            // Closer is not added, as it will be added in OnRaised when the latch is opened for
+            // the first time.
+            openTrigger.Add(this);
+        }
+
+        private void OnDisable()
+        {
+            openTrigger.Remove(this);
+            closeTrigger.Remove(this);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void OnRaised(SimpleArg arg)
+        {
+            if (closed)
+            {
+                // Opening.
+                openTrigger.Remove(this);
+                closeTrigger.Add(this);
+                onOpen.Invoke();
+            }
+            else
+            {
+                // Closing.
+                openTrigger.Add(this);
+                closeTrigger.Remove(this);
+                onClose.Invoke();
+            }
+
+            closed = !closed;
+        }
+
+        #endregion
+    }
+}
