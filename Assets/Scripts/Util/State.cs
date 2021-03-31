820f55dcbe95f586ada13657a19eabbb9883a43b
Edvinas <edvinas108@gmail.com>
2021-03-31 14:11:19 +0300

    Add game states & events

---
+++ Assets/Scripts/Util/State.cs
@@ -0,0 +1,79 @@
+﻿using ScriptableEvents;
+using ScriptableEvents.Simple;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NeanderthalTools.Util
+{
+    public class State : MonoBehaviour, IScriptableEventListener<SimpleArg>
+    {
+        #region Editor
+
+        [SerializeField]
+        private SimpleScriptableEvent nextStateTrigger;
+
+        [SerializeField]
+        private State nextState;
+
+        [SerializeField]
+        private UnityEvent onEnter;
+
+        [SerializeField]
+        private UnityEvent onExit;
+
+        #endregion
+
+        #region Unity Lifecycle
+
+        private void OnEnable()
+        {
+            StartState();
+
+            if (nextStateTrigger == null)
+            {
+                return;
+            }
+
+            nextStateTrigger.Add(this);
+        }
+
+        private void OnDisable()
+        {
+            StartNextState();
+
+            if (nextStateTrigger == null)
+            {
+                return;
+            }
+
+            nextStateTrigger.Remove(this);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void OnRaised(SimpleArg arg)
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void StartState()
+        {
+            onEnter.Invoke();
+        }
+
+        private void StartNextState()
+        {
+            onExit.Invoke();
+            if (nextState != null)
+            {
+                nextState.gameObject.SetActive(true);
+            }
+
+            gameObject.SetActive(false);
+        }
+
+        #endregion
+    }
+}
