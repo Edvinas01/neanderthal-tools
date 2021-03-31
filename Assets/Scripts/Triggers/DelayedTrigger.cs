f971ec9355d5e1ccf630b3f13ee2bb8c7db39b5a
Edvinas <edvinas108@gmail.com>
2021-03-31 14:32:36 +0300

    Add util trigger, update scene to fire the first state

---
+++ Assets/Scripts/Triggers/DelayedTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NeanderthalTools.Triggers
+{
+    public class DelayedTrigger : MonoBehaviour
+    {
+        #region Editor
+
+        [Min(0f)]
+        [SerializeField]
+        [Tooltip("Delay in seconds")]
+        private float delay = 1f;
+
+        [SerializeField]
+        private UnityEvent onTrigger;
+
+        #endregion
+
+        #region Unity Lifecycle
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(delay);
+            onTrigger.Invoke();
+            Destroy(this);
+        }
+
+        #endregion
+    }
+}
