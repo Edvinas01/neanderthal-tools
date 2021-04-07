b9e5e903089932c42e647a736b53f5ef7a338db4
Edvinas <edvinas108@gmail.com>
2021-04-07 12:00:51 +0300

    Add state scriptable events

---
+++ Assets/Scripts/States/StateScriptableEvent.cs
@@ -0,0 +1,13 @@
+﻿using ScriptableEvents;
+using UnityEngine;
+
+namespace NeanderthalTools.States
+{
+    [CreateAssetMenu(
+        fileName = "StateScriptableEvent",
+        menuName = "Game/Scriptable Events/State Scriptable Event"
+    )]
+    public class StateScriptableEvent : BaseScriptableEvent<StateEventArgs>
+    {
+    }
+}
