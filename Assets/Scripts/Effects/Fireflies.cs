ddb3e658bd7355502089ef0de6b9cfca512eace4
Edvinas <edvinas108@gmail.com>
2021-04-18 17:20:46 +0300

    Add fireflies

---
+++ Assets/Scripts/Effects/Fireflies.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using NeanderthalTools.Util;
+using UnityEngine;
+
+namespace NeanderthalTools.Effects
+{
+    public class Fireflies : MonoBehaviour
+    {
+        #region Editor
+
+        [SerializeField]
+        private float fadeInDuration = 1f;
+
+        [SerializeField]
+        private float fadeOutDuration = 1f;
+
+        #endregion
+
+        #region Fields
+
+        private new Light light;
+        private float initialLightIntensity;
+
+        private new ParticleSystem particleSystem;
+
+        #endregion
+
+        #region Unity Lifecylce
+
+        private void Awake()
+        {
+            light = GetComponentInChildren<Light>();
+            initialLightIntensity = light.intensity;
+
+            particleSystem = GetComponentInChildren<ParticleSystem>();
+        }
+
+        private void Start()
+        {
+            SetLightActive(false);
+            SetEmissionActive(false);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void StartFadeIn()
+        {
+            SetEmissionActive(true);
+            StartCoroutine(FadeInLight());
+        }
+
+        public void StartFadeOut()
+        {
+            SetEmissionActive(false);
+            StartCoroutine(FadeOutLight());
+        }
+
+        private void SetLightActive(bool value)
+        {
+            light.gameObject.SetActive(value);
+        }
+
+        private void SetEmissionActive(bool value)
+        {
+            var emissionModule = particleSystem.emission;
+            emissionModule.enabled = value;
+        }
+
+        private IEnumerator FadeInLight()
+        {
+            SetLightActive(true);
+            yield return Coroutines.Progress(
+                0f,
+                initialLightIntensity,
+                fadeInDuration,
+                SetLightIntensity
+            );
+        }
+
+        private IEnumerator FadeOutLight()
+        {
+            yield return Coroutines.Progress(
+                initialLightIntensity,
+                0f,
+                fadeOutDuration,
+                SetLightIntensity
+            );
+
+            SetLightActive(false);
+        }
+
+        private void SetLightIntensity(float value)
+        {
+            light.intensity = value;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Effects/FireflyFader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NeanderthalTools.Effects
+{
+    public class FireflyFader : MonoBehaviour
+    {
+        #region Fields
+
+        private readonly List<Fireflies> fireflies = new List<Fireflies>();
+
+        #endregion
+
+        #region Unity Lifecycle
+
+        private void Awake()
+        {
+            fireflies.AddRange(GetComponentsInChildren<Fireflies>());
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void StartFadeIn()
+        {
+            foreach (var firefly in fireflies)
+            {
+                firefly.StartFadeIn();
+            }
+        }
+
+        public void StartFadeOut()
+        {
+            foreach (var firefly in fireflies)
+            {
+                firefly.StartFadeOut();
+            }
+        }
+
+        #endregion
+    }
+}
