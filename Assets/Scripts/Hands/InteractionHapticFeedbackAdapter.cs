f2630ed1bae238a60eefa2c59fc0886dcdf32aa0
Edvinas <edvinas108@gmail.com>
2021-04-20 15:38:34 +0300

    Add haptics to hafting, prep script for interaction haptics

---
+++ Assets/Scripts/Hands/InteractionHapticFeedbackAdapter.cs
@@ -0,0 +1,25 @@
+﻿using NeanderthalTools.Haptics;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace NeanderthalTools.Hands
+{
+    public class InteractionHapticFeedbackAdapter : MonoBehaviour
+    {
+        #region Editor
+
+        [SerializeField]
+        private HapticFeedbackSettings settings;
+
+        #endregion
+
+        #region Methods
+
+        public void SendHapticImpulse(BaseInteractionEventArgs args)
+        {
+            settings.SendHapticImpulse(args.interactor);
+        }
+
+        #endregion
+    }
+}
