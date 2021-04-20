3a93f3e5089181c6874aeb9cfb838c600399fff8
Edvinas <edvinas108@gmail.com>
2021-04-20 15:49:13 +0300

    Add haptics to interactables

--- Assets/Scripts/Hands/InteractionHapticFeedbackAdapter.cs
+++ Assets/Scripts/Hands/InteractionHapticFeedbackAdapter.cs
@@ -11,6 +11,51 @@ namespace NeanderthalTools.Hands
         [SerializeField]
         private HapticFeedbackSettings settings;
 
+        [SerializeField]
+        private XRBaseInteractable interactable;
+
+        [SerializeField]
+        [Tooltip("Adapt grab events")]
+        private bool isAdaptSelectEntered;
+
+        [SerializeField]
+        [Tooltip("Adapt hover events")]
+        private bool isAdaptHoverEntered;
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Unity Lifecycle
+
+        private void OnEnable()
+        {
+            if (isAdaptSelectEntered)
+            {
+                interactable.selectEntered.AddListener(SendHapticImpulse);
+            }
+
+            if (isAdaptHoverEntered)
+            {
+                interactable.hoverEntered.AddListener(SendHapticImpulse);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (isAdaptSelectEntered)
+            {
+                interactable.selectEntered.RemoveListener(SendHapticImpulse);
+            }
+
+            if (isAdaptHoverEntered)
+            {
+                interactable.hoverEntered.RemoveListener(SendHapticImpulse);
+            }
+        }
+
         #endregion
 
         #region Methods
