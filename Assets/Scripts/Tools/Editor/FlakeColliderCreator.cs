385856ca1f970acafd0be522e38bcfdf69e44f77
Edvinas <edvinas108@gmail.com>
2021-04-21 12:02:09 +0300

    Add flake collider creater

---
+++ Assets/Scripts/Tools/Editor/FlakeColliderCreator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace NeanderthalTools.Tools.Editor
+{
+    public class FlakeColliderCreator : EditorWindow
+    {
+        #region Unity Lifecycle
+
+        [MenuItem("GameObject/Create Flake Collider")]
+        private static void CreateCollider(MenuCommand menuCommand)
+        {
+            var gameObject = menuCommand.context as GameObject;
+            var mesh = GetSharedMesh(gameObject);
+
+            if (mesh == null)
+            {
+                return;
+            }
+
+            CreateCollider(gameObject, mesh);
+        }
+
+        [MenuItem("GameObject/Create Flake Collider", true)]
+        private static bool IsCreateCollider()
+        {
+            return GetSharedMesh(Selection.activeObject as GameObject) != null;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static Mesh GetSharedMesh(GameObject gameObject)
+        {
+            if (gameObject == null)
+            {
+                return null;
+            }
+
+            var meshFilter = gameObject.GetComponent<MeshFilter>();
+            if (meshFilter == null)
+            {
+                return null;
+            }
+
+            return meshFilter.sharedMesh;
+        }
+
+        private static void CreateCollider(GameObject parentGameObject, Mesh mesh)
+        {
+            var parentTransform = parentGameObject.transform;
+            var gameObject = new GameObject
+            {
+                transform =
+                {
+                    position = parentTransform.position,
+                    rotation = parentTransform.rotation,
+                    parent = parentTransform
+                },
+                name = "Collider"
+            };
+
+            var meshCollider = gameObject.AddComponent<MeshCollider>();
+            meshCollider.sharedMesh = mesh;
+            meshCollider.convex = true;
+        }
+
+        #endregion
+    }
+}
