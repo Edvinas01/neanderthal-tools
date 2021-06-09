76b45d1b3561df4b486adc6bf3a14bd34a3baa8b
Edvinas <edvinas108@gmail.com>
2021-06-09 10:45:29 +0300

    Add handle highlight script and material

---
+++ Assets/Scripts/Effects/HandleHighlight.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace NeanderthalTools.Effects
+{
+    [RequireComponent(typeof(Renderer))]
+    public class HandleHighlight : MonoBehaviour
+    {
+        #region Editor
+
+        [SerializeField]
+        private Material highlightMaterial;
+
+        #endregion
+
+        #region Fields
+
+        private new Renderer renderer;
+        private Material originalMaterial;
+
+        #endregion
+
+        #region Unity Lifecycle
+
+        private void Awake()
+        {
+            renderer = GetComponent<Renderer>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void ShowHighlight()
+        {
+            if (originalMaterial == null)
+            {
+                originalMaterial = renderer.material;
+            }
+
+            renderer.material = highlightMaterial;
+        }
+
+        public void ClearHighlight()
+        {
+            renderer.material = originalMaterial;
+        }
+
+        #endregion
+    }
+}
